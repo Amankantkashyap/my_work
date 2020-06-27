33fb14edcd5317e1ff89155fa29364653860b4e1
Aman Kant Kashyap <akant641@gmail.com>
2020-06-28 01:28:52 +0530

    Update Nimesa_assignment.java

--- Nimesa_assignment.java
+++ Nimesa_assignment.java
@@ -1,3 +1,25 @@
+//Problem
+
+/*https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&appid=b6907d289e10d714a6e88b30761fae22
+The above API is the REST GET API
+Which gives you the response in JSON format and hourly weather forecast of London Location
+
+I want you to write a program to get the option from the user and print the result based on the above API.
+1. Get weather
+2. Get Wind Speed
+3. Get Pressure
+0. Exit
+
+If the user press 1, Prompt the user for the date then print the temp of the input date.
+If the user press 2, Prompt the user for the date then print the wind.speed of the input date.
+If the user press 3, Prompt the user for the date then print the pressure of the input date.
+If the user press 0, Terminate the program.
+
+The program should not terminate until the user press 0.
+The program should be modular.*/
+
+//Solution in Java
+
 package Nimesa_Assignments;
 
 import  java.net.HttpURLConnection;
